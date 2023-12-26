f7812e1f2aa58b62ce7b86a73712062d9c6cb469
apasalic10 <115503793+apasalic10@users.noreply.github.com>
2023-12-26 10:14:13 +0100

    fixed menu design and behavior

--- public/scripts/updateMenu.js
+++ public/scripts/updateMenu.js
@@ -1,7 +1,9 @@
-let prijavaListItem,odjavaMenuItem,meni;
+let prijavaListItem,odjavaMenuItem,meni,profilMenuItem;
 window.onload = function (){
     meni = document.getElementById("menu-screen");
     prijavaListItem = meni.contentWindow.document.getElementById("prijavaItem");
+    profilMenuItem = meni.contentWindow.document.getElementById("profilItem");
+    profilMenuItem.style.display = "none";
     PoziviAjax.getKorisnik(updateMenuSuccessLogin);
 }
 
@@ -9,6 +11,7 @@ window.onload = function (){
 function updateMenuSuccessLogin(err,data){
     if(data){
         let odjavaHTML = "<a id='meniOdjava' href='prijava.html' target='#'>Odjava</a>";
+        profilMenuItem.style.display = "inline-block";
         prijavaListItem.innerHTML = odjavaHTML;
 
         odjavaMenuItem = meni.contentWindow.document.getElementById("meniOdjava");
@@ -21,6 +24,7 @@ function updateMenuSuccessLogin(err,data){
 function updateLogout(err,data){
     if(data){
         let prijavaHTML = "<a id='meniPrijava' href='prijava.html' target='#'>Prijava</a>";
+        profilMenuItem.style.display = "none";
         prijavaListItem.innerHTML = prijavaHTML;
     }
 }
